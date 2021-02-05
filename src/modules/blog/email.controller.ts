7b4d28565789795c0cbc5fff4a8feabfdb24d0f8
Anil Sharma <anilsharma@Anils-MacBook-Air.local>
2021-02-05 12:33:19 +0530

    email service added

--- src/modules/blog/email.controller.ts
+++ src/modules/blog/email.controller.ts
@@ -15,14 +15,73 @@ export class EmailController {
      */
     @Get('/contact')
     async getArticles(@Req() req: Request) {
+
+        const name = req.query.name || 'Anil Sharma';
+        const subject = req.query.subject || 'Hello ✔ from Ani!';
+        const email = req.query.email || 'anela.kumar@gmail.com';
+        const phone = req.query.phone || '+91 8860327209';
+        const message = req.query.phone || 'HoLLa!!';
+
         let data = {
-            from: "91wheelsservicealert@gmail.com",
+            from: `${name} <${email}>`,
             to: "anela.kumar@gmail.com,ratidigital.in@gmail.com",
-            subject: "Hello ✔", // Subject line
-            text: "Hello world?", // plain text body
-            html: "<b>Hello world?</b>", // html body
+            subject: `Hello ✔ from : ${name}`,
+            text: `${message}`,
+            html: this.getHtml(name, subject, email, phone, message),
         }
         const resp = await this.emailService.send(data);
         return resp;
+    }
+    //function get html
+    getHtml(name: any, subject: any, email: any, phone: any, message: any) {
+        const content =
+            `<!DOCTYPE html>
+            <html>
+            <head>
+            <style>
+            table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+            }
+            td, th {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+            }
+            tr:nth-child(even) {
+            background-color: #dddddd;
+            }
+            </style>
+            </head>
+            <body>
+            <p><i>${subject}</i></p>
+            </hr>
+            <h2>Basic Information</h2>
+            <table>
+            <tr>
+                <th>Identifiers</th>
+                <th>Value</th>
+            </tr>
+            <tr>
+                <td>Name</td>
+                <td>${name}</td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>${email}</td>
+            </tr>
+            <tr>
+                <td>Phone</td>
+                <td>${phone}</td>
+            </tr>
+            </table>
+            </hr>
+            <h2>Message:</h2>
+            <p><i>${message}</i></p>
+            </body>
+            </html>
+            `;
+        return content;
     }
 }
